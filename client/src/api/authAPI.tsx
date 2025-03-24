eb5aa3bf8737f0451025cbf53a0edbd7a8279579
Matt Mullen <mhmullen@icloud.com>
2025-03-24 16:31:28 +1200

    added authAPI, installed npm

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,15 +2,20 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
-  const response = await fetch("XXXXXXXXXXXXXXXXXX", {
+  const response = await fetch("api/user", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Accept: "application/json",
+      credentials: "include",
     },
     body: JSON.stringify(userInfo),
   });
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.message);
+  }
+  return data;
 }
-
-
 
 export { login };
